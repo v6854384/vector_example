e95cffee90d189e390d3a7690fc97ffc11a774d1
justcppdev <justcppdev@gmail.com>
2018-03-12 13:06:24 +0300

    refactored

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -5,24 +5,24 @@
 
 TEST_CASE("creating vector")
 {
-    vector_t vector;
-    REQUIRE( vector.size() == 0 );
-    REQUIRE( vector.capacity() == 0 );
+	vector_t vector;
+	REQUIRE( vector.size() == 0 );
+	REQUIRE( vector.capacity() == 0 );
 }
 
 TEST_CASE("copying vector")
 {
-    vector_t vector;
+	vector_t vector;
 	vector.push_back(1);
 
-    vector_t copy(vector);
-    REQUIRE( copy == vector );
+	vector_t copy(vector);
+	REQUIRE( copy == vector );
 }
 
 TEST_CASE("assigning vector")
 {
-    vector_t vector1;
-    vector_t vector2;
+	vector_t vector1;
+	vector_t vector2;
 
 	vector1.push_back(1);
 	vector2.push_back(2);
@@ -33,8 +33,8 @@ TEST_CASE("assigning vector")
 
 TEST_CASE("equaling vector")
 {
-    vector_t vector1;
-    vector_t vector2;
+	vector_t vector1;
+	vector_t vector2;
 
 	vector1.push_back(1);
 	vector2.push_back(1);
@@ -47,13 +47,13 @@ TEST_CASE("equaling vector")
 
 TEST_CASE("indexing vector")
 {
-    vector_t vector;
+	vector_t vector;
 
 	vector.push_back(1);
 
 	REQUIRE( vector[0] == 1 );
 
-    vector_t const copy(vector);
+	vector_t const copy(vector);
 	REQUIRE( copy[0] == 1 );
 }
 
@@ -62,24 +62,24 @@ TEST_CASE("pushing elements")
 	vector_t vector;
 
 	vector.push_back(1);
-    REQUIRE( vector.size() == 1 );
-    REQUIRE( vector.capacity() == 1 );
+	REQUIRE( vector.size() == 1 );
+	REQUIRE( vector.capacity() == 1 );
 
 	vector.push_back(2);
-    REQUIRE( vector.size() == 2 );
-    REQUIRE( vector.capacity() == 2 );
+	REQUIRE( vector.size() == 2 );
+	REQUIRE( vector.capacity() == 2 );
 
 	vector.push_back(3);
-    REQUIRE( vector.size() == 3 );
-    REQUIRE( vector.capacity() == 4 );
-	
+	REQUIRE( vector.size() == 3 );
+	REQUIRE( vector.capacity() == 4 );
+
 	vector.push_back(4);
-    REQUIRE( vector.size() == 4 );
-    REQUIRE( vector.capacity() == 4 );
+	REQUIRE( vector.size() == 4 );
+	REQUIRE( vector.capacity() == 4 );
 
 	vector.push_back(5);
-    REQUIRE( vector.size() == 5 );
-    REQUIRE( vector.capacity() == 8 );
+	REQUIRE( vector.size() == 5 );
+	REQUIRE( vector.capacity() == 8 );
 }
 
 TEST_CASE("poping elements")
@@ -94,26 +94,26 @@ TEST_CASE("poping elements")
 	vector.push_back(6);
 
 	vector.pop_back();
-    REQUIRE( vector.size() == 5 );
-    REQUIRE( vector.capacity() == 8 );
+	REQUIRE( vector.size() == 5 );
+	REQUIRE( vector.capacity() == 8 );
 
 	vector.pop_back();
-    REQUIRE( vector.size() == 4 );
-    REQUIRE( vector.capacity() == 8 );
+	REQUIRE( vector.size() == 4 );
+	REQUIRE( vector.capacity() == 8 );
 
 	vector.pop_back();
-    REQUIRE( vector.size() == 3 );
-    REQUIRE( vector.capacity() == 8 );
+	REQUIRE( vector.size() == 3 );
+	REQUIRE( vector.capacity() == 8 );
 
 	vector.pop_back();
-    REQUIRE( vector.size() == 2 );
-    REQUIRE( vector.capacity() == 4 );
+	REQUIRE( vector.size() == 2 );
+	REQUIRE( vector.capacity() == 4 );
 
 	vector.pop_back();
-    REQUIRE( vector.size() == 1 );
-    REQUIRE( vector.capacity() == 2 );
+	REQUIRE( vector.size() == 1 );
+	REQUIRE( vector.capacity() == 2 );
 
 	vector.pop_back();
-    REQUIRE( vector.size() == 0 );
-    REQUIRE( vector.capacity() == 1 );
+	REQUIRE( vector.size() == 0 );
+	REQUIRE( vector.capacity() == 1 );
 }
