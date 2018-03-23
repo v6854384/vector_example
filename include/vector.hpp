1d3735d1834aa8eae762ef75d12beda4da132952
v6854384 <32385043+v6854384@users.noreply.github.com>
2018-03-23 12:34:46 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -3,20 +3,20 @@
 class vector_t
 {
 private:
-	int * elements_;
-	std::size_t size_;
-	std::size_t capacity_;
+	int * elements_; //указатель на динамический массив
+	std::size_t size_; //количество элементов
+	std::size_t capacity_; //количсетво выделенной памяти
 public:
 	vector_t();
 	vector_t(vector_t const & other);
 	vector_t & operator =(vector_t const & other);
 	~vector_t();
 
-	std::size_t size() const;
+	std::size_t size() const; //возвращают значения полей
 	std::size_t capacity() const;
 
-	void push_back(int value);
-	void pop_back();
+	void push_back(int value); //добавление элемента
+	void pop_back(); //удаление элемента (если элементов в четыре раза меньше, то сужаем в два раза размер массива
 
 	int & operator [](std::size_t index);
 	int operator [](std::size_t index) const;
